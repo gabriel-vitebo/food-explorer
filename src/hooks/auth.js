2acf4da234240f14df370a76cec00f44365218c0
gabriel-vitebo <vitebo@outlook.com>
2023-05-08 14:42:39 -0300

    creating authentication hook

---
+++ src/hooks/auth.js
@@ -0,0 +1,9 @@
+import { createContext } from "react"
+
+export const AuthContext = createContext({})
+
+function AuthProvider({ children }) {
+  return <AuthContext.Provider>{children}</AuthContext.Provider>
+}
+
+export { AuthProvider }
