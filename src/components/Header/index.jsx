c795de1441e3a8d60c71851b6c331e463e099016
gabriel-vitebo <vitebo@outlook.com>
2023-05-09 13:05:30 -0300

    adjusting the header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,9 +1,15 @@
+import { useState } from "react"
+
 import { Container } from "./styles"
 import { TfiReceipt } from "react-icons/tfi"
 import { MenuButton } from "../MenuButton"
 import { Logo } from "../Logo"
+import { Input } from "../Input"
+import { Button } from "../Button"
 
-export function Header({ amount, isAdm }) {
+import { AiOutlineSearch, AiOutlineClose } from "react-icons/ai"
+
+export function Header({ amount, isAdm, isActive }) {
   return (
     <Container>
       <MenuButton />
@@ -16,6 +22,19 @@ export function Header({ amount, isAdm }) {
           <span>{amount}</span>
         </div>
       )}
+      <div className="menu">
+        <div className="close-menu">
+          <Button icon={AiOutlineClose} title={"Menu"} />
+        </div>
+        <Input
+          icon={AiOutlineSearch}
+          placeholder={"Busque por pratos ou ingredientes"}
+        />
+        <ul>
+          <li>Sair</li>
+          {isAdm && <li>Novo prato</li>}
+        </ul>
+      </div>
     </Container>
   )
 }
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -9,6 +9,7 @@ export const Container = styled.header`
   justify-content: space-between;
   align-items: center;
   background-color: ${({ theme }) => theme.COLORS.dark700};
+  position: relative;
 
   > svg {
     color: ${({ theme }) => theme.COLORS.light100};
@@ -45,6 +46,49 @@ export const Container = styled.header`
       position: absolute;
       right: 1.8rem;
       top: 3.5rem;
+    }
+  }
+
+  > .menu {
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: 1;
+    height: 100vh;
+    background-color: ${({ theme }) => theme.COLORS.dark700};
+    padding: 2.8rem;
+    width: 100%;
+    > .close-menu {
+      height: 11.4rem;
+      display: flex;
+
+      a {
+        font-size: 2.4rem;
+        font-family: ${({ theme }) => theme.FONTS.Roboto};
+        font-weight: 400;
+        color: ${({ theme }) => theme.COLORS.light100};
+
+        svg {
+          width: 2.4rem;
+          height: 2.4rem;
+        }
+      }
+    }
+
+    ul {
+      list-style: none;
+
+      font-size: 2.4rem;
+      font-family: ${({ theme }) => theme.FONTS.Poppins};
+      font-weight: 400;
+      color: ${({ theme }) => theme.COLORS.light300};
+
+      margin-top: 4.5rem;
+
+      li {
+        border-bottom: 1px solid ${({ theme }) => theme.COLORS.dark1000};
+        padding: 0 0 1rem 0;
+      }
     }
   }
 `
