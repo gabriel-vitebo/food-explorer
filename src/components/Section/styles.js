c6f701650463b0949e81d8cd86355665870e7a08
gabriel-vitebo <vitebo@outlook.com>
2023-06-29 11:37:33 -0300

    adding animation to sections

--- src/components/Section/styles.js
+++ src/components/Section/styles.js
@@ -1,7 +1,32 @@
-import styled from "styled-components"
+import styled, { keyframes } from "styled-components";
+
+const slide = keyframes`
+   0% {
+        opacity: 0;
+        transform: translateY(-3.3rem);
+    }
+
+    100% {
+        opacity: 1;
+        transform: translateY(0);
+    }
+`;
+
+const dropDown = keyframes`
+  0% {
+        opacity: 0;
+        transform: translatex(-3.3rem);
+    }
+
+    100% {
+        opacity: 1;
+        transform: translatex(0);
+    }
+`;
 
 export const Container = styled.section`
   > h2 {
+    animation: ${slide} 0.6s 0.3s backwards;
     font-family: ${({ theme }) => theme.FONTS.Poppins};
     font-weight: 500;
     font-size: 1.8rem;
@@ -9,9 +34,10 @@ export const Container = styled.section`
   }
 
   > div {
+    animation: ${dropDown} 0.6s 0.3s backwards;
     display: flex;
     flex-wrap: nowrap;
     gap: 1.6rem;
     overflow-x: scroll;
   }
-`
+`;
